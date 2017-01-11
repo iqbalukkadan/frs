f3048ee8ac3f1a3d5bf0d90e9521408f02bef297
Iqbal Ukkadan <iqbalukkadan@gmail.com>
2017-01-11 15:53:56 +0530

    setup our project....

---
+++ application/views/pages/home.php
@@ -0,0 +1,39 @@
+<header>
+    <div class="container-fluid top-area">
+        <div class="container">
+            <div class="col-sm-12">
+                <div class="col-sm-2 contact-nomber">
+                     <i class="fa fa-headphones" aria-hidden="true"></i>
+                        <span>+91 9911172261</span>
+                </div>
+                <div class="col-sm-4">
+                    <ul>
+                        <li>
+                            <i class="fa fa-facebook" aria-hidden="true"></i>
+
+                        </li>
+                        <li>
+                            <i class="fa fa-linkedin" aria-hidden="true"></i>
+
+                        </li>
+                        <li>
+                            <i class="fa fa-instagram" aria-hidden="true"></i>
+
+                        </li>
+                        <li>
+                            <i class="fa fa-google-plus" aria-hidden="true"></i>
+
+                        </li>
+                        <li>
+                           <i class="fa fa-youtube-play" aria-hidden="true"></i>
+
+                        </li>
+                       
+                        
+                    </ul>
+                </div>
+            <div class="col-sm-6"></div>
+            </div>
+        </div>
+    </div>
+</header> 
---
+++ application/views/templates/header.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <script src="<?php echo SCRIPTPATH ?>/jquery-1.11.3.js" type="text/javascript"></script>
+        <script src="<?php echo SCRIPTPATH ?>/frs.js" type="text/javascript"></script>
+        <script src="<?php echo COMPONENTPATH ?>/bootstrap-3.3.7-dist/js/bootstrap.js" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href="<?php echo CSSPATH ?>/frs.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo CSSPATH ?>/font-awesome.css">
+        <link rel="stylesheet" type="text/css" href="<?php echo COMPONENTPATH ?>/bootstrap-3.3.7-dist/css/bootstrap.css">
+    </head>
+    <body>
+        
